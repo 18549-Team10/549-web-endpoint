ef526fa57e90b3f11390893681f440be16f3dff7
Evans Hauser <evanshauser@gmail.com>
2017-03-20 12:42:40 -0400

    Added parameter to file write

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,31 @@
 var express = require('express');
+var fs  = require('fs');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  fs.readFile('/tmp/output', 'utf8', function (err,data) {
+    if (err) {
+      return console.log(err);
+    }
+    console.log(data);
+    recentFreq = data;
+
+    res.render('index', { title: 'Main' });
+  });
+});
+
+router.get('/query', function(req, res, next) {
+  fs.appendFile("/tmp/output", req.query.freq + "\n", function(err) {
+    if(err) {
+      return console.log(err);
+    }
+    console.log("The file was saved!");
+  });
+
+  recentFreq = req.query.freq;
+
+  res.render('index', { title: 'Query' });
 });
 
 module.exports = router;
