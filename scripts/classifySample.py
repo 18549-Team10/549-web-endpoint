7b1685544706cdd2358eb2b25de06b00976e5db1
Selin Sirinterlikci <ssirinte@andrew.cmu.edu>
2017-05-02 18:45:38 -0400

    Began to implement unique matching algorithm in score function

--- scripts/classifySample.py
+++ scripts/classifySample.py
@@ -3,29 +3,40 @@
 # given: single sample's peaks from container with unknown amount of liquid, map created from training data
 # goal: classify sample with best guess of amount of liquid in container
 
-def bestMatch(freq, mag, mapPeaks):
+def bestMatch(freq, mag, mapPeaks, prevPeaksMatched):
     # currently, matches only based on frequencies, but can be changed to 
     # match based on magnitude as well
     minDist = None
     bestFreq, bestMag = None, None
+    secondBestFreq, secondBestMag = None, None
+    print("Map Peaks = " + str(mapPeaks))
     for (mapFreq, mapMag) in mapPeaks:
         if mapFreq == None: continue
         dist = abs(mapFreq - freq)
-        if minDist == None or dist < minDist:
+        if (minDist == None or dist < minDist) and dist not in prevPeaksMatched:
             minDist = dist
+            if (bestFreq != None and bestMag != None):
+                secondBestFreq = bestFreq
+                secondBestMag = bestMag
             bestFreq, bestMag = mapFreq, mapMag
+            prevPeaksMatched.append(bestFreq)
+    if dist in prevPeaksMatched:
+        bestFreq, bestMag = secondBestFreq, secondBestMag
+    print("Best Freq = " + str(bestFreq))
+    print("Best Mag = " + str(bestMag))
     return bestFreq, bestMag
 
 def score(samplePeaks, mapPeaks, ratio, debug):
     # currently, only adds to score based on closeness of frequencies, but can
     # be modified to score based on similarity of magnitude as wells
     peakScores = []
+    prevPeaksMatched = []
     for (freq,mag) in samplePeaks:
-        matchFreq,matchMag = bestMatch(freq,mag, mapPeaks)
+        matchFreq,matchMag = bestMatch(freq,mag, mapPeaks, prevPeaksMatched)
         freqDiff = abs(freq - matchFreq)
         magDiff = abs(mag - matchMag)
         if debug: print freqDiff, magDiff
-        peakScores.append(freqDiff)
+        peakScores.append(magDiff)
     peakScores.pop(peakScores.index(max(peakScores))) # .28, .48 with this line, .23, .48 without
     return sum(peakScores) / len(samplePeaks)
 
