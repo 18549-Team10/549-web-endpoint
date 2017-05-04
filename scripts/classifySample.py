72772ae7a37bdf08846bd46ca1a94d95236f6485
Selin Sirinterlikci <ssirinte@andrew.cmu.edu>
2017-05-04 14:27:09 -0400

    Added debugging statements and first match to classifySample.py

--- scripts/classifySample.py
+++ scripts/classifySample.py
@@ -10,18 +10,30 @@ def bestMatch(freq, mag, mapPeaks):
     # match based on magnitude as well
     minDist = None
     bestFreq, bestMag = None, None
+    firstIteration = True
     for (mapFreq, mapMag) in mapPeaks:
         if mapFreq == None: continue
-        dist = abs(mapFreq - freq)
-        minDist = dist
-        bestFreq, bestMag = mapFreq, mapMag
+        freqDist = abs(mapFreq - freq)
+        magDist = abs(mapMag - mag)
+        dist = math.sqrt(pow(freqDist, 2) + pow(magDist, 2))
+        print("MapFreq = " + str(mapFreq) + "|| MapMag = " + str(mapMag))
+        print("Dist = " + str(dist))
+        if firstIteration or dist < minDist:
+            minDist = dist
+            bestFreq, bestMag = mapFreq, mapMag
+            firstIteration = False
+        print("Best Freq = " + str(bestFreq) + "|| Best Mag = " + str(bestMag))
     return bestFreq, bestMag
 
 def score(samplePeaks, mapPeaks, ratio, debug):
     # currently, only adds to score based on closeness of frequencies, but can
     # be modified to score based on similarity of magnitude as wells
+    # TODO: Make this recursive, find the best match and make that pairing. Take them out of 
+    # the options array when you've made a match
     peakScores = []
     for (freq,mag) in samplePeaks:
+        print("----------------------------------------")
+        print("Trying a new frequency and mag from my sample...")
         matchFreq,matchMag = bestMatch(freq,mag, mapPeaks)
         freqDiff = abs(freq - matchFreq)
         magDiff = abs(mag - matchMag)
