a5dd1826ccbc0f1a2e919596b09e569d414dc19a
Nicholas Wilson <Nicholas Wilson>
2017-05-06 03:30:10 -0400

    debugging info added

--- scripts/rawToFill.py
+++ scripts/rawToFill.py
@@ -120,11 +120,11 @@ else:
     debug = True
 
 if DO_KEG:
-    print "keg!"
+    print "keg!", sys.arv[1]
     import classifySample as cs
     import fingerprinter as fp
 else:
-    print "waterbottle."
+    print "waterbottle.", sys.arv[1]
     import wb_classifySample as cs
     import wb_fingerprinter as fp
 
